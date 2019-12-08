1a5f35bc11bea5d9ab04abfcdeef9e04b9c471d0
karim kohel <kareemkohel@gmail.com>
2019-12-08 20:14:30 +0200

    refactored the code intirely to adapt for the new feature : the abililty to specify the number of times to scale down the image, updating the help string accrodingly

--- scaledown.py
+++ scaledown.py
@@ -6,33 +6,76 @@ import sys
 
 helpString = """
 pyramid is a simple program to scale down images
-usage : pyramids <image to scale down> <destination>
 
-there is no other options yet
+USAGE : pyramids <image to scale down> [options]
+
+OPTIONS :
+	-n <path or name to produced image>
+			the name or path of the produced image
+
+	-t <number of times to scale down>
+			the programe will scale down the image by a factor of 1/2 the number of times specified
 """
 
 ########## FX ##########
 
-def check():
+def check_input():
 	if sys.argv[1] == "--help":
 		print(helpString)
 		sys.exit()
 
-	elif len(sys.argv) < 3:
-		print(f"Usage pyramids <file to use> <destination>")
+	elif 2 > len(sys.argv) > 4:
+		print("Usage : pyramids <image to scale down> [options]")
+		print("for help use the --help flag")
 		sys.exit()
 
-def save(lower_reselution, name):
-	cv2.imwrite(name, lower_reselution)
+def check_times():
+	if "-t" in sys.argv:
+		index = sys.argv.index("-t") + 1
+		return sys.argv[index]
+	else:
+		return 0
+
+def check_name():
+	try:
+		filename, extention = sys.argv[1].split(".")
+	except Exception:
+		print("Error loading image")
+		sys.exit()
+	else:
+		if "-n" in sys.argv:
+			index = sys.argv.index("-n") + 1
+
+			if extention in sys.argv[index]:
+				name = sys.argv[index]
+			else:
+				name = sys.argv[index] + "." + extention
+		
+		else:
+			name = filename[0] + "2." + extention
+		return name
+
+def get_img():
+	try:
+		img = cv2.imread(sys.argv[1])
+	except Exception:
+		print("Error loading image, check image file")
+		sys.exit()
+	else:
+		return img
 
 ########## Main ##########
 
-check()
+check_input()
+img = get_img()
+times = int(check_times())
+destination = check_name()
 
-img = cv2.imread(sys.argv[1])
+if times > 0:
+	times -= 1
+	for i in range(times):
+		img = cv2.pyrDown(img)
+else:
+	img = cv2.pyrDown(img)
 
-lr = cv2.pyrDown(img)
-
-destination = sys.argv[2]
-
-save(lr, destination)
+cv2.imwrite(destination, img)
