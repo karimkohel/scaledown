7f9b19f2db873f67ee6596d12bb2ab049b5262dd
karim kohel <kareemkohel@gmail.com>
2019-12-08 15:37:14 +0200

    finished the program and will test soon

--- scaledown.py
+++ scaledown.py
@@ -2,36 +2,37 @@
 import cv2
 import sys
 
-########## FX ##########
+########## init ##########
 
-def check():
-	if sys.argv[1] == "--help":
-		print("""
+helpString = """
 pyramid is a simple program to scale down images
 usage : pyramids <image to scale down> <destination>
 
 there is no other options yet
-""")
+"""
+
+########## FX ##########
+
+def check():
+	if sys.argv[1] == "--help":
+		print(helpString)
 		sys.exit()
 
 	elif len(sys.argv) < 3:
 		print(f"Usage pyramids <file to use> <destination>")
 		sys.exit()
 
-def save(lower_reselution):
-	cv2
+def save(lower_reselution, name):
+	cv2.imwrite(name, img)
 
 ########## Main ##########
 
 check()
 
 img = cv2.imread(sys.argv[1])
+
 lr = cv2.pyrDown(img)
+
 destination = sys.argv[2]
 
-
-
-########## clean up ##########
-
-cv2.waitKey()
-cv2.destroyAllWindows()
+save(lr, destination)
